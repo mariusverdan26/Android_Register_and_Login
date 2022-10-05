e09624f71ca147b95991e527e5b42b49c9f0bde9
blnkcnvs <mariusverdan26@gmail.com>
2022-10-05 11:19:17 +0800

    Changes in Main

--- app/src/main/java/com/capstone/androidregisterndlogin/MainActivity.java
+++ app/src/main/java/com/capstone/androidregisterndlogin/MainActivity.java
@@ -1,14 +1,75 @@
 package com.capstone.androidregisterndlogin;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name, email, password, c_password;
+    private Button btn_regist;
+    private ProgressBar loading;
+    private static String URL_REGIST = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loading = findViewById(R.id.loading);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        c_password = findViewById(R.id.c_password);
+        btn_regist = findViewById(R.id.btn_regist);
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility(View.GONE);
+
+        final String name = this.name.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                })
+        {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
     }
 }
